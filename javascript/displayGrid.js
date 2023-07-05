bd174b1a74aba028aa48f3c10dbf6bbe4cb57e05
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-05 14:36:47 +0100

    added styling to the tile grid
    Relates #1

--- javascript/displayGrid.js
+++ javascript/displayGrid.js
@@ -1,6 +1,6 @@
 export function displayGridTiles() {
     const gridContainer = document.getElementById('tiles-container');
-    console.log(gridContainer)
+   
     const numberOfTiles = 30;
 
     for (let i = 0; i < numberOfTiles; i++){
