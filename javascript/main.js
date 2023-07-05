4f8e26bdd95918009f91d669f0032ae7ede88813
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-05 16:12:53 +0100

    added the keys for the keyboard
    Relates #1

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,9 @@
 import { displayGridTiles } from "./displayGrid.js";
 
+import { displayKeyboard } from "./displayKeyboard.js";
+// Display the tiles using JS
 displayGridTiles()
+
+//Display the keyboard using JS
+
+displayKeyboard()
