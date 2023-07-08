f5f16196f30e16b451413115acd540a69b3d195b
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-08 20:35:09 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+aws s3 cp index.html s3://elenas-wordle
+aws s3 cp 404.html s3://elenas-wordle
+aws s3 cp javascript s3://elenas-wordle/javascript --recursive
+aws s3 cp stylesheets s3://elenas-wordle/stylesheets --recursive
+
+# it would be much better if we could do this :)
+# aws s3 cp website s3://elenas-wordle --recursive
