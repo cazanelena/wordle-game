c3a349a71b2b3eb4397a75f92fb3028e180565fa
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-08 21:06:26 +0100

    added favicon

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ aws s3 cp index.html s3://elenas-wordle
 aws s3 cp 404.html s3://elenas-wordle
 aws s3 cp javascript s3://elenas-wordle/javascript --recursive
 aws s3 cp stylesheets s3://elenas-wordle/stylesheets --recursive
+aws s3 cp images s3://elenas-wordle/images --recursive
 
 # it would be much better if we could do this :)
 # aws s3 cp website s3://elenas-wordle --recursive
